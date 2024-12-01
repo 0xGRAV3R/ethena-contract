363b352829ca7a5bb657b1e12c62ee3b65e3ad57
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-12-01 14:58:15 -0800

    🚢 Add imports IERC20 & Ownable

--- contracts/EthenaPredictionMarket.sol
+++ contracts/EthenaPredictionMarket.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
 contract EthenaPredictionMarket {
     constructor() {}
 }
