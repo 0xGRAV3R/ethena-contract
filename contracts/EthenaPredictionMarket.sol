9642074231a9a7d09d9bd0febb1682b2f39de4e8
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-12-01 15:02:58 -0800

    🚢 Add imports ReentrancyGuard

--- contracts/EthenaPredictionMarket.sol
+++ contracts/EthenaPredictionMarket.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract EthenaPredictionMarket {
     constructor() {}
