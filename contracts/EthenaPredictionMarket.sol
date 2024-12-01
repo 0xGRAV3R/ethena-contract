a8d896c7859447acf286bfc7be65d5adc9755f87
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-12-01 15:17:52 -0800

    🐙 Create markets variable which is going to be our index for Market itself

--- contracts/EthenaPredictionMarket.sol
+++ contracts/EthenaPredictionMarket.sol
@@ -29,7 +29,7 @@ contract EthenaPredictionMarket is Ownable, ReentrancyGuard {
 
     IERC20 public bettingToken;
     uint256 public marketCount;
-    
+    mapping(uint256 => Market) public markets;
 
     constructor() {}
 
