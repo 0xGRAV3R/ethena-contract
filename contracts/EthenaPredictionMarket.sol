9d0c202e026a5d82bab38f838670964898df7927
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-12-01 16:10:50 -0800

    🍇 Fixing dependency errors

--- contracts/EthenaPredictionMarket.sol
+++ contracts/EthenaPredictionMarket.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract EthenaPredictionMarket is ERC20, Ownable, ReentrancyGuard {
     enum MarketOutcome {
@@ -27,7 +28,7 @@ contract EthenaPredictionMarket is ERC20, Ownable, ReentrancyGuard {
         mapping(address => bool) hasClaimed;
     }
 
-    IERC20 public bettingToken;
+    ERC20 public bettingToken;
     uint256 public marketCount;
     mapping(uint256 => Market) public markets;
 
@@ -40,7 +41,7 @@ contract EthenaPredictionMarket is ERC20, Ownable, ReentrancyGuard {
     );
 
     event SharesPurchased(
-        uint256 indexed merketId,
+        uint256 indexed marketId,
         address indexed buyer,
         bool isOptionA,
         uint256 amount
